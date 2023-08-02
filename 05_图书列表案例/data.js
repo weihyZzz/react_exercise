71eca57bfe56ced91a3fabce7e844b0b9d321de3
weihy <971973918@qq.com>
2023-08-02 20:10:34 +0800

    jsx本质，图书案例部分完成

---
+++ 05_图书列表案例/data.js
@@ -0,0 +1,5 @@
+const books = [
+    {id: 1, name: 'react学习手册', price: 18, count: 1},
+    {id: 2, name: '你不知道的JavaScript', price: 20, count: 1},
+    {id: 1, name: 'Node.js实战', price: 10, count: 1}
+]
