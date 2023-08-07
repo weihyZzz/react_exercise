39bce2c58a47a9891a5d97065f9edf6d66147af7
weihy <971973918@qq.com>
2023-08-07 11:25:55 +0800

    补充额外生命周期函数

--- 03_learn_component/src/02_react生命周期/HelloWorld.jsx
+++ 03_learn_component/src/02_react生命周期/HelloWorld.jsx
@@ -28,12 +28,31 @@ class HelloWorld extends React.Component {
         // 3.执行componentDidMount ，组件被渲染到DOM，挂载到DOM
         console.log('HelloWorld组件： 执行componentDidMount');
     }
-    componentDidUpdate() {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        // 在组件更新后被调用，表示组件的DOM已经更新完成
+        // 接收三个参数：prevProps、prevState和snapshot。
+        // 它们分别代表组件更新前的旧属性（props）、旧状态（state）以及从getSnapshotBeforeUpdate返回的快照信息。
         // 4.如果执行了setState函数，则触发DOM更新
         console.log('HelloWorld组件： 执行componentDidUpdate');
+        console.log('prevProps', prevProps, 'prevState', prevState, 'snapshot', snapshot);
     }
     componentWillUnmount() {
         console.log('HelloWorld组件： 执行componentWillUnmount');
+    }
+
+    shouldComponentUpdate() {
+        // 允许手动控制组件在更新过程中是否要重新渲染
+        console.log('HelloWorld组件： 执行shouldComponentUpdate');
+        return true
+    }
+
+    getSnapshotBeforeUpdate() {
+        // 在组件即将更新到DOM中之前被调用，允许你捕获当前DOM状态，并在更新后使用这些信息。
+        console.log('HelloWorld组件： 执行getSnapshotBeforeUpdate');
+        return {
+            // 例如可以在某应用场景下缓存滚动的位置
+            scrollPosition: 100
+        }
     }
 }
 
