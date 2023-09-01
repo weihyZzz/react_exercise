327e1cd7a14650fd10fbc85babd03d1273f36f88
weihy <971973918@qq.com>
2023-09-01 21:17:16 +0800

    演示styled中样式的继承

--- 04_learn_react_css/src/05_CSS_in_js写法/home/Home.jsx
+++ 04_learn_react_css/src/05_CSS_in_js写法/home/Home.jsx
@@ -1,10 +1,14 @@
 import React, { PureComponent } from "react";
-import { HomeWrapper } from "./style";
+import { HomeWrapper, ButtonWrapper, PlusButtonWrapper } from "./style";
 export class Home extends PureComponent {
   render() {
     return (
       <HomeWrapper>
         <div className="title">Home Page</div>
+
+        {/* 演示styled中样式的继承 */}
+        <ButtonWrapper>按钮1</ButtonWrapper>
+        <PlusButtonWrapper>按钮升级版</PlusButtonWrapper>
       </HomeWrapper>
     );
   }
--- 04_learn_react_css/src/05_CSS_in_js写法/home/style.js
+++ 04_learn_react_css/src/05_CSS_in_js写法/home/style.js
@@ -1,5 +1,15 @@
 import { styled } from "styled-components"
 
+export const ButtonWrapper = styled.button`
+    border: 1px solid green;
+    border-radius: 5px;
+`
+// styled中样式的继承
+export const PlusButtonWrapper = styled(ButtonWrapper)`
+    background-color: pink;
+`
+
+
 export const HomeWrapper = styled.div`
     .title {
         /* 使用styled-components中的ThemeProvider提供的数据 */
