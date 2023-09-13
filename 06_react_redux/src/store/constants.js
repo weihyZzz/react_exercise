6a7e49a9422600224ffeeb63aa84ba5f59838d59
weihy <971973918@qq.com>
2023-09-13 21:13:20 +0800

    模拟编写日志中间件

--- 06_react_redux/src/store/constants.js
+++
@@ -1,4 +0,0 @@
-export const ADD_NUMBER = "add_number"
-export const SUB_NUMBER = "sub_number"
-export const CHANGE_GODDS = "change_goods"
-export const CHANGE_USER = "change_user"
--- 06_react_redux/src/store/index.js
+++ 06_react_redux/src/store/index.js
@@ -21,4 +21,23 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace:true
 // 创建store
 // const store = createStore(reducer, applyMiddleware(thunk))
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+// 模拟日志中间件
+function log(store) {
+    // 记录旧dispatch
+    const next = store.dispatch
+    
+    function logAndDispatch(action) {
+        console.log('当前派发action', action);
+        // 真正进行action派发（使用旧dispatch）
+        next(action)
+        console.log('派发之后的结果', store.getState());
+    }
+    
+    // monkey patch
+    store.dispatch = logAndDispatch
+}
+log(store)
+
+
 export default store
