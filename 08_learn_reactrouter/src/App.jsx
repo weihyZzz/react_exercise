9c8bfb7f90c07d488b3beb6d43994823e0df6e33
weihy <971973918@qq.com>
2023-09-17 19:22:37 +0800

    在函数式组件中使用useNavigate来实现js方式路由跳转

--- 08_learn_reactrouter/src/App.jsx
+++ 08_learn_reactrouter/src/App.jsx
@@ -1,60 +1,64 @@
-import React, { PureComponent } from "react";
-import { Routes, Route, Link, NavLink, Navigate } from "react-router-dom";
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link,
+  NavLink,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Notfound from "./pages/Notfound";
 import HomeRecommends from "./pages/HomeRecommends";
 import HomeRanking from "./pages/HomeRanking";
-export class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogin: false,
-    };
+import Category from "./pages/Category";
+import Order from "./pages/Order";
+export function App(props) {
+  const navigate = useNavigate();
+  function navigateTo(path) {
+    navigate(path);
   }
-  setLogin() {
-    this.setState({ isLogin: true });
-  }
-  render() {
-    // const { isLogin } = this.state;
-    return (
-      <div className="app">
-        <div className="header">
-          <h2>header</h2>
-          <div className="nav">
-            <Link to="/home">首页</Link>
-            <Link to="/about">关于</Link>
-
-            <div className="navlink">
-              <span>NavLink组：</span>
-              <NavLink to="/home">首页</NavLink>
-              <NavLink to="/about">关于</NavLink>
-              <NavLink onClick={(e) => this.setLogin()}>登录</NavLink>
-            </div>
+  return (
+    <div className="app">
+      <div className="header">
+        <h2>header</h2>
+        <div className="nav">
+          <Link to="/home">首页</Link>
+          <Link to="/about">关于</Link>
+          {/* 通过useNavigate实现js代码路由跳转 */}
+          <button onClick={(e) => navigateTo("/category")}>分类</button>
+          <span onClick={(e) => navigateTo("/order")}>订单</span>
+          <div className="navlink">
+            <span>NavLink组：</span>
+            <NavLink to="/home">首页</NavLink>
+            <NavLink to="/about">关于</NavLink>
           </div>
-          <hr />
         </div>
-        <div className="content">
-          {/* 配置映射关系， path - Component */}
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<Home />}>
-              {/* 配置嵌套路由 */}
-              <Route path="/home" element={<Navigate to="/home/recommend" />} />
-              <Route path="/home/recommend" element={<HomeRecommends />} />
-              <Route path="/home/ranking" element={<HomeRanking />} />
-            </Route>
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Notfound />} />
-          </Routes>
-        </div>
-        <div className="footer">
-          <hr />
-          <h2>footer</h2>
-        </div>
+        <hr />
       </div>
-    );
-  }
+      <div className="content">
+        {/* 配置映射关系， path - Component */}
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />}>
+            {/* 配置嵌套路由 */}
+            <Route path="/home" element={<Navigate to="/home/recommend" />} />
+            <Route path="/home/recommend" element={<HomeRecommends />} />
+            <Route path="/home/ranking" element={<HomeRanking />} />
+          </Route>
+          <Route path="/about" element={<About />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </div>
+      <div className="footer">
+        <hr />
+        <h2>footer</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App;
