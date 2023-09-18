b7d36847a98f90d35680dcc4e4dfa398771cdec3
weihy <971973918@qq.com>
2023-09-18 10:48:59 +0800

    使用useParams，根据动态路由传递的参数展示具体页面

--- 08_learn_reactrouter/src/hoc/withRouter.js
+++ 08_learn_reactrouter/src/hoc/withRouter.js
@@ -1,11 +1,14 @@
 // 高阶组件 withRouter: 用于在类组件中使用react-router的hook函数
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 function withRouter(WrapperComponent) {
     return function(props) {
         const navigate = useNavigate()
-        
+        const params = useParams()
+        console.log('navigate', navigate);
+        console.log('params', params);
+
         // 包装要传递给原始组件的参数
-        const router = { navigate }
+        const router = { navigate, params }
         return <WrapperComponent {...props} router={router} />
     }
 }
--- 08_learn_reactrouter/src/pages/Detail.jsx
+++ 08_learn_reactrouter/src/pages/Detail.jsx
@@ -1,14 +1,16 @@
 import React, { PureComponent } from "react";
-
+import withRouter from "../hoc/withRouter";
 export class Detail extends PureComponent {
   render() {
+    const { router } = this.props;
+    const { params } = router;
     return (
       <div>
         <h2>详情页</h2>
-        <h3>歌曲id:{111}</h3>
+        <h3>歌曲id:{params.id}</h3>
       </div>
     );
   }
 }
 
-export default Detail;
+export default withRouter(Detail);
